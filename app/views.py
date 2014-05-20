11a9a73eead59117e03de083c5ad2571f3f3ea30
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-05-20 14:37:28 -0400

    Configured Heroku diployment

--- app/views.py
+++ app/views.py
@@ -8,5 +8,16 @@ from app import app
 from flask import render_template
 
 @app.route('/')
+@app.route('/index')
 def index():
 	return render_template('3Divs2.html')
+
+@app.route('/account')
+@app.route('/account_page')
+def account():
+	return render_template('3Divs2Account.html')
+
+@app.route('/profile')
+@app.route('/profile_page')
+def profile():
+	return render_template('3Divs2Profile.html')
---
+++ runp-heroku.py
@@ -0,0 +1,10 @@
+#============================================================
+#runp-heroku.py 
+#a script that launches the app
+#============================================================
+# Have the application recognize different characters used in different languages
+# -*- coding: utf-8 -*-
+#!flask/bin/python
+
+
+from app import app
